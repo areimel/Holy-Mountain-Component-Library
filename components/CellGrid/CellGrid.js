44793f85998bd7b822f92d80406eec703dfd9555
Alec Reimel <alecreimel1@gmail.com>
2021-03-14 20:24:09 -0400

    netlify build update

--- components/CellGrid/CellGrid.js
+++ components/CellGrid/CellGrid.js
@@ -6,7 +6,11 @@ const Cell = styled.div`
 	padding: ${props => props.padding || "25px"};
 
 	.inner{
-		max-width: ${props => props.cellInnerWidth || "350px"};
+		max-width: ${props => props.cellInnerWidth || "450px"};
+	}
+
+	@media only screen and (max-width : 1200px) { 
+	  	width: 100%;
 	}
   }
 
@@ -23,6 +27,7 @@ export default function CellGrid({
 	  <Cell 
 	    className={styles.cell}
 	    columns={columns}
+	    cellInnerWidth={cellInnerWidth}
 	  >
             <div className={styles.inner +' inner'} cellInnerWidth={cellInnerWidth}>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
@@ -32,6 +37,7 @@ export default function CellGrid({
 	  <Cell 
 	    className={styles.cell}
 	    columns={columns}
+	    cellInnerWidth={cellInnerWidth}
 	  >
             <div className={styles.inner +' inner'} cellInnerWidth={cellInnerWidth}>
               Lorem ipsum dolor sit amet, 
@@ -41,6 +47,7 @@ export default function CellGrid({
 	  <Cell 
 	    className={styles.cell}
 	    columns={columns}
+	    cellInnerWidth={cellInnerWidth}
 	  >
             <div className={styles.inner +' inner'} cellInnerWidth={cellInnerWidth}>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.
